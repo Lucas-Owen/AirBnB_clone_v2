1a6185672682212108be3616d0720b797e172941
Lucas Sangura <sanguraowens@gmail.com>
2023-10-06 17:24:22 +0300

    Rewrite file redirection to run as root when sudo is used

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # This bash script configures an ubuntu machine to serve a static webpage
-apt-get -y update
+apt-get update
 apt-get -y upgrade
 apt install -y nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 touch /data/web_static/releases/test/index.html
-echo -e '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>' > /data/web_static/releases/test/index.html
+bash -c "echo -e '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>' > /data/web_static/releases/test/index.html"
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 sed -i '/server_name _;/a\\n\tlocation /hbnb_static \{\n\t\talias /data/web_static/current/;\n\t\}' /etc/nginx/sites-available/default
-nginx -s reload
+service nginx restart
