f21d0143183caf4f1b6d6a97a6500cb88a7cc34d
Lucas Sangura <sanguraowens@gmail.com>
2023-10-20 10:28:33 +0300

    Add route to /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,5 +14,11 @@ def hello_HBNB():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb")
+def HBNB():
+    """This function returns 'HBNB'"""
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run()
