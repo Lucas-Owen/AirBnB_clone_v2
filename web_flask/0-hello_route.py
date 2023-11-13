c67f394447e47acc5285d3a33376b35eb8a88529
Lucas Sangura <sanguraowens@gmail.com>
2023-11-13 08:41:06 +0300

    Change path for 0-hllo_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/airbnb-onepage/")
 def hello_HBNB():
     """This function returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
